a7080a628435ebfb3fa3d9f3e49506511144bdb7
misterussell <max@misterussell.com>
2019-07-05 13:52:19 -0500

    configuring initial cognito integration

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignUp = () => {
-  return (
-    <div className="sign-up">
-      Sign Up form to go here.
-    </div>
-  );
-};
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: null,
+      confirm: null,
+      confirmationCode: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+  };
 
+  render() {
+    return(
+      <div className="sign-up">
+        <form>
+          <p>email:</p>
+          <input
+            value={this.state.email}
+            onChange={e => this.onChange('email', e.target.value)}
+            placeholder="email"
+          />
+          <p>password:</p>
+          <input
+            value={this.state.password}
+            onChange={e => this.onChange('password', e.target.value)}
+            placeholder="password"
+          />
+          <button onClick={this.handleSubmit}>sign up</button>
+        </form>
+      </div>
+    );
+  };
+
+  onChange(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const email = this.state.email.trim();
+    const password = this.state.password.trim();
+    // sign up account
+  }
+
+  handleConfirm(e) {
+    e.preventDefault();
+    const email = this.state.email.trim();
+    const code = this.state.confirmationCode.trim();
+    // confirm account
+  }
+
+}
 export default SignUp;
