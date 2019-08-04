e78d8d32f24f55bb588713bcceea61002cef2fad
misterussell <max@misterussell.com>
2019-08-04 18:01:13 -0400

    adding Activity class and defining MVP of an activity

---
+++ src/models/Activity.jsx
@@ -0,0 +1,22 @@
+export default class Activity {
+  /*
+    this will handle all activity methods
+    todo: provision a SQL db for these
+    MVP structure for an event:
+      {
+        id: unique ID,
+        calendarID: unique ID referencing the Calendar the activity is saved to, (defaults to a general calendar)
+        title: str,
+        description: str,
+        externalURL: url,
+        date: date,
+        startAt: date/time
+        endAt: date/time
+        allDay: bool,
+        private: bool,
+        archived: bool,
+        archivedAt: date/time
+        tags: arr, (no more than 5 tags can be added to an activity, these are SQl db entries so can be shared between activities)
+      }
+  */
+}
